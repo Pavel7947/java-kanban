a2f9b0b866b154d1a20060949ce2c8b7a4d38904
Pavel Syutkin <syutkinpi@yandex.ru>
2024-06-30 00:11:39 +0500

    Remove the whitespace in the manager class

--- src/service/Manager.java
+++ src/service/Manager.java
@@ -4,9 +4,10 @@ public class Manager {
 
     private Manager(){}
 
-    public static TaskManager getDefault() {
+    public static TaskManager getDefault(){
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory(){
         return new InMemoryHistoryManager();
     }
